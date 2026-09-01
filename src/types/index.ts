c4d04c6756213273f399368e22604e8fc20435db
Aditya Salgotra <adityasalgotra@Adityas-MacBook-Air.local>
2026-09-02 05:22:04 +0530

    fix(types): add unit and price optional fields to Item interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -76,7 +76,10 @@ export interface Item {
   description?: string;
   pricePerKg?: number;
   pricePerItem?: number;
+  price?: number;
+  unit?: 'KG' | 'ITEM';
 }
+
 
 export interface OrderItem {
   itemId: string;
